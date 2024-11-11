7c637f73b28ed58f281b6c597fc6c0402ed8cf0d
Eva025 <94.05.25jj@gmail.com>
2024-11-11 16:31:28 +0800

    第二次

--- app/src/main/java/com/example/gesture/MainActivity.kt
+++ app/src/main/java/com/example/gesture/MainActivity.kt
@@ -20,9 +20,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 
 
 class MainActivity : ComponentActivity() {
@@ -75,23 +84,89 @@ fun PointerEvents() {
 fun Tap() {
 
     var msg by remember { mutableStateOf("TAP相關手勢實例") }
+    var count by remember { mutableStateOf(0) }
+    var offset1 by remember { mutableStateOf(Offset.Zero) }
+    var offset2 by remember { mutableStateOf(Offset.Zero) }
+    var PU = arrayListOf(R.drawable.pu0, R.drawable.pu1,
+        R.drawable.pu2, R.drawable.pu3,
+        R.drawable.pu4, R.drawable.pu5)
+    var Number by remember { mutableStateOf(0) }
 
     Column {
+        Text("\n" + msg+"\n計數:" +count.toString(),
+            modifier  = Modifier.pointerInput(Unit){
+                detectTapGestures(
+                    onPress = {count=0}
+                )
+            }
+        )
 
-        Text("\n" + msg)
+        Box(modifier = Modifier
+        .background(Color.Yellow)
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+
+                detectDragGesturesAfterLongPress(
+                    onDrag = { change, dragAmount -> offset2+=dragAmount},
+                    onDragStart = {offset1 = it
+                                   offset2 = it},
+                    onDragEnd = {
+                        if (offset2.x >= offset1.x){
+                            msg = "長按後向右拖曳"
+                            Number ++
+                            if (Number>5){Number=0}
+                        }
+                        else{
+                            msg = "長按後向左拖曳"
+                            Number --
+                            if (Number<0){Number=5}
+                        }
+
+                    },
+
+                    )
+
+            }
+        ){
+            Text("")
+        }
 
         Image(
-
-            painter = painterResource(id = R.drawable.pu0),
-
+            painter = painterResource(id =  PU[Number]),
             contentDescription = "靜宜之美",
-
             modifier = Modifier
-
                 .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onTap = {msg = "短按"
+                                 count++},
+                        onDoubleTap = {msg = "雙擊"
+                                      count+=3},
+                        onLongPress = {msg = "長按"
+                                      count+=2},
+                        onPress = {msg = "先觸發onPress(按下)"}
+                    )
+
+                }
 
         )
 
     }
 
+}
+
+@Composable
+fun Drag_Horizontal() {
+    var offsetX by remember { mutableStateOf(0f) }
+    Text(
+        text = "水平拖曳",
+        modifier = Modifier
+            .offset { IntOffset(offsetX.toInt(), 130) }
+            .draggable(
+                orientation= Orientation.Horizontal,
+                state = rememberDraggableState{ delta ->
+                    offsetX += delta
+                }
+            )
+    )
 }
